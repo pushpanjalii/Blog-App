c18222dac741d2850f4749278b35f99d5e4b3055
pushpanjali <vermapushpanjali5@gmail.com>
2024-11-01 14:17:56 +0530

    user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const User = require('../models/User.js')
+const User = require('../models/user.js')
 const bcrypt = require('bcrypt')
 const post = require('./post.js')
 const comment = require('./comment.js')
